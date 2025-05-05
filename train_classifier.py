68a9550f617c735a71f1d3d7f674fc1402c44335
James <jamesw162@hotmail.co.uk>
2025-05-05 11:52:16 +0100

    Commented train_classifier

--- train_classifier.py
+++ train_classifier.py
@@ -1,16 +1,15 @@
-import os
+#import os
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 
-# Specify the full absolute path to the data.pickle file
-#data_pickle_path = os.path.join(os.path.dirname(os.path.abspath("C:/Users/willd/Downloads/Group9Project-main/Group9Project-main")), 'data.pickle')
+# Specify the full relative path to the data.pickle file
 data_pickle_path = r"data.pickle"
 data_dict = pickle.load(open(data_pickle_path, 'rb'))
 
-# The dataset now has samples with 84 datapoints (2 hands, 42 per hand)
+# The dataset has samples with 84 datapoints (2 hands, 42 per hand)
 data = np.asarray(data_dict['data'])
 labels = np.asarray(data_dict['labels'])
 
@@ -19,12 +18,15 @@ x_train, x_test, y_train, y_test = train_test_split(
     data, labels, test_size=0.2, shuffle=True, stratify=labels
 )
 
+# Define model algorithm as a Random Forest Classifier. 
 model = RandomForestClassifier()
 model.fit(x_train, y_train)
 
+# Test result against the initial dataset. Outputs success rate as a percentage.
 y_predict = model.predict(x_test)
 score = accuracy_score(y_predict, y_test)
 print('{}% of samples were classified correctly'.format(score*100))
 
-with open('model.p', 'wb') as f:  # Save the trained model.
+# Save the trained model.
+with open('model.p', 'wb') as f:  
     pickle.dump({'model': model}, f)
